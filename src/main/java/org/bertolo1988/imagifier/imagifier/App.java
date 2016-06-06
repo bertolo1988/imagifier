21c32493cc104be13f965e0e56587152c772e66a
BERTOLO Tiago <tiago.bertolo@slib.com>
2016-06-06 13:34:58 +0100

    first commit - created maven project

---
+++ src/main/java/org/bertolo1988/imagifier/imagifier/App.java
@@ -0,0 +1,13 @@
+package org.bertolo1988.imagifier.imagifier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
